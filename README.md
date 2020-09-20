343d486ee78d8ee8277682e51a503181415c215c
Zach Taylor <zachtylr04@gmail.com>
2020-09-20 15:13:22 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# django-react-starter
+
+This is a basic set up to serve a React single-page application from Django
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const BundleTracker = require("webpack-bundle-tracker");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    frontend: "./frontend/src/index.js",
+  },
+  output: {
+    path: path.resolve("./frontend/static/frontend/"),
+    filename: "[name]-[hash].js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleTracker({
+      path: __dirname,
+      filename: "./webpack-stats.json",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
